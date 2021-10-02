f35a324fcde202eb9a9c13461ae8bcf9569c2a30
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2021-10-02 20:01:05 +0530

    direction check

---
+++ sketch/sketch.ino
@@ -0,0 +1,19 @@
+int in1 = 0;
+int in2 = 0;
+int en = 0;
+
+
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(en, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(en, HIGH);
+  delay(1000);
+  digitalWrite(en, LOW);
+  delay(1000);
+}
